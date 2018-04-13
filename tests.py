12478354db1020ae8493c4ec5312744d903739ef
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-13 14:37:21 -0500

    feat: Add tests for question2 function

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from question1 import question1
+from question2 import question2
 
 
 class TestSolutions(TestCase):
@@ -7,3 +8,7 @@ class TestSolutions(TestCase):
     def test_question_one(self):
         self.assertEquals(False, question1("ababa", "fa"))
         self.assertEquals(True, question1("udacity", "ad"))
+
+    def test_question_two(self):
+        self.assertEquals("aba", question2("abaandaba"))
+        self.assertEquals("", question2("dart"))
