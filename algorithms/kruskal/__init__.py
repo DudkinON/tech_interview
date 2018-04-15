d8237c291ed89c7b7a89f27972a50b402461dd69
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-15 01:27:56 -0500

    feat: Add kruskal function

---
+++ algorithms/kruskal/__init__.py
@@ -0,0 +1,18 @@
+def kruskal(sets, edges):
+    graph = {}
+
+    for edge in edges:
+
+        # if there is only one spanning tree left, break the loop
+        if len(sets) == 1:
+            break
+
+        # find indexes of vertices
+        set_one = find_set(edge[1])
+        set_two = find_set(edge[2])
+
+        # if vertices are not in same spanning tree, merge them
+        if set_one != set_two:
+            union(min(set_one, set_two), max(set_one, set_two), edge)
+
+    return graph
