fd3571f1131bfc83b7b4bc905f4dee63ea8d4973
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-19 03:03:42 -0500

    feat: Add class List to linked_list package

--- algorithms/linked_list/__init__.py
+++ algorithms/linked_list/__init__.py
@@ -2,3 +2,11 @@ class Node(object):
     def __init__(self, data):
         self.data = data
         self.next = None
+
+
+class List(object):
+    def __init__(self, r):
+        self.root = Node(r)
+        self.end = self.root
+        self.current = self.root
+        self.length = 1
