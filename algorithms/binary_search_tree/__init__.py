43e002fd8b8af4084321d911c1a36fd250e9c3ae
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-19 01:52:38 -0500

    feat: Add method search_helper to BST class

--- algorithms/binary_search_tree/__init__.py
+++ algorithms/binary_search_tree/__init__.py
@@ -33,3 +33,13 @@ class BST(object):
 
     def search(self, find_val):
         return self.search_helper(self.root, find_val)
+
+    def search_helper(self, current, find_val):
+        if current:
+            if current.value == find_val:
+                return True
+            elif current.value < find_val:
+                return self.search_helper(current.right, find_val)
+            else:
+                return self.search_helper(current.left, find_val)
+        return False
