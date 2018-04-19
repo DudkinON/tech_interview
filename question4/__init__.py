00060af550fe2054e6a8390c8f570206bbcb7980
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-19 01:43:40 -0500

    feat: Add read_matrix function

--- question4/__init__.py
+++ question4/__init__.py
@@ -2,6 +2,24 @@ from algorithms.binary_search_tree import BST
 
 
 def question4(m, r, n1, n2):
+    def read_matrix(m):
+        binary_tree = None
+        for i in range(len(m)):
+            for j in range(len(m[i])):
+                if m[i][j] == 1:
+                    if binary_tree is None:
+                        binary_tree = BST(i)
+                        binary_tree.insert(j)
+                    else:
+                        if j == binary_tree.root.value:
+                            temp_tree = binary_tree
+                            binary_tree = BST(i)
+                            binary_tree.insert(j)
+                            binary_tree.add(temp_tree)
+                        else:
+                            if binary_tree.root.value == i:
+                                binary_tree.insert(j)
+        return binary_tree
 
     tree = read_matrix(m)
     assert tree.root.value == r
