8dd36cc719f7d8b3679604b1c6a13cdfe39ec26f
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-19 02:24:54 -0500

    feat: Remove excessive code

--- algorithms/binary_search_tree/__init__.py
+++ algorithms/binary_search_tree/__init__.py
@@ -77,27 +77,18 @@ class BST(object):
                 return self.search_helper(current.left, find_val)
         return False
 
-    def pre_order(self, node, target):
-        if node:
-            if node.value == target:
-                return True
-            else:
-                return (self.pre_order(node.left, target) or
-                        self.pre_order(node.right, target))
-        return False
-
     def search_parent_helper(self, n1, n2, node):
         if node:
             if node.value == n1 or node.value == n2:
                 return node.value
 
             if node.left is not None and node.right is not None:
-                if (self.pre_order(node.left, n1) and
-                        self.pre_order(node.right, n2) or
-                        self.pre_order(node.left, n2) and
-                        self.pre_order(node.right, n1)):
+                if (self.search_helper(node.left, n1) and
+                        self.search_helper(node.right, n2) or
+                        self.search_helper(node.left, n2) and
+                        self.search_helper(node.right, n1)):
                     return node.value
-                elif self.pre_order(node.left, n1):
+                elif self.search_helper(node.left, n1):
                     return self.search_parent_helper(n1, n2, node.left)
                 else:
                     return self.search_parent_helper(n1, n2, node.right)
