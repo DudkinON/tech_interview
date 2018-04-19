a472a527f47c85923e608faf39f2ee4fbfb1120b
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-19 01:48:01 -0500

    feat: Add insert mathod to BST class

--- algorithms/binary_search_tree/__init__.py
+++ algorithms/binary_search_tree/__init__.py
@@ -9,3 +9,6 @@ class BST(object):
     def __init__(self, root):
         self.root = Node(root)
         self.current_parent = self.root
+
+    def insert(self, new_val):
+        self.insert_helper(self.root, new_val)
