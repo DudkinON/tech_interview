c12b3d14e0a89dc2fec309f69d1b947dddf036d2
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-19 01:42:52 -0500

    feat: Add question4 function

--- question4/__init__.py
+++ question4/__init__.py
@@ -1,5 +1,21 @@
-class Node(object):
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+from algorithms.binary_search_tree import BST
+
+
+def question4(m, r, n1, n2):
+
+    tree = read_matrix(m)
+    assert tree.root.value == r
+    return tree.search_parent(n1, n2)
+
+
+def main():
+    matrix = [[0, 1, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [1, 0, 0, 0, 1],
+              [0, 0, 0, 0, 0]]
+    print question4(matrix, 3, 1, 4)
+
+
+if __name__ == '__main__':
+    main()
